f5733e3bf6379f1371377d2dfa21a9820369d497
webcrawls <itskadenhere@gmail.com>
2026-02-06 12:36:12 -0500

    closer to the style of my teacher's requested output - string formatting like this is a weak point for me

--- exercise_5_2_2.py
+++ exercise_5_2_2.py
@@ -1,5 +1,8 @@
 def convert_to_km(mile: int):
     return mile * 1.609
 
-for i in range(1, 11):
-    print(convert_to_km(1))
+print(f"{'Miles':^10}{'Kilometers':^12}")
+print('---------------------')
+for mile in range(1, 11):
+    km = convert_to_km(mile) 
+    print(f"{mile:^10}{km:^12.3f}")
